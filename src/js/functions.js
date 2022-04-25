f51e1791dd4b1a8bc24ff688940ef95cd0bf33d3
Burak <48869708+drementer@users.noreply.github.com>
2022-04-25 07:54:53 +0300

    yeni fonskyionlar eklendi

--- src/js/functions.js
+++ src/js/functions.js
@@ -4,9 +4,42 @@ let scrool_kapat = () => {
 	},
 	scrool_ac = () => {
 		document.querySelector("html, body").style.overflowY = "auto";
-	};
+	},
+	log = console.log.bind(document),
+	isString = (value) => {
+		return typeof value === "string" || value instanceof String;
+	},
+	isNull = (value) => {
+		return value !== null || value !== undefined;
+	},
+	isNumber = (value) => {
+		return !isNaN(value);
+	},
+	goToTop = () => window.scrollTo(0, 0),
+	isDarkMode =
+		window.matchMedia &&
+		window.matchMedia("(prefers-color-scheme: dark)").matches;
 /* Genel SON */
 
+/*  */
+///		(function () {
+///			some private code that will be executed automatically
+///		})();
+///		(function (a, b) {
+///			var result = a + b;
+///			return result;
+///		})(10, 20);
+/*  */
+
 /* Export */
-export { scrool_ac, scrool_kapat };
+export {
+	scrool_ac,
+	scrool_kapat,
+	log,
+	isString,
+	isNull,
+	isNumber,
+	goToTop,
+	isDarkMode,
+};
 /* Export SON */
