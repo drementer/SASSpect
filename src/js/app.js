1b87b683e54aad1fab6b51509f009ee3189cea4e
Burak <48869708+drementer@users.noreply.github.com>
2022-05-19 04:18:59 +0300

    importlar alındı ve atamalar yapıldı

--- src/js/app.js
+++ src/js/app.js
@@ -15,14 +15,27 @@ import {
 	is_number,
 	go_top,
 	is_dark_mode,
-	pozisyon
+	pozisyon,
 } from "./__functions.js";
 
 // Çerez
-import cerez from "./__cerez_popup.js";
+import cerez from "./__cerez-popup.js";
 cerez();
+
+// Sliders
+import cerez from "./__sliders.js";
+Sliders();
 
 // Lazy Load
 import lazy_load from "./__io.js";
 lazy_load();
+
+// Form Elemanları
+import form_elemanlari from "./__form-elemanlari.js";
+form_elemanlari();
+
 /* İmport SON */
+
+/* Atamalar */
+const doc = document,
+	body = doc.body;
