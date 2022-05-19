c778f05ebdc3b70925e93a308adc520fb27be947
Burak <48869708+drementer@users.noreply.github.com>
2022-05-19 15:36:34 +0300

    yazım hatası giderildi ve mf eklendi

--- src/js/app.js
+++ src/js/app.js
@@ -23,8 +23,8 @@ import cerez from "./__cerez-popup.js";
 cerez();
 
 // Sliders
-import cerez from "./__sliders.js";
-Sliders();
+import sliders from "./__sliders.js";
+sliders();
 
 // Lazy Load
 import lazy_load from "./__io.js";
@@ -33,6 +33,48 @@ lazy_load();
 // Form Elemanları
 import form_elemanlari from "./__form-elemanlari.js";
 form_elemanlari();
+
+// Mouse Follower
+import MouseFollower from "./__mouse-follower.js";
+MouseFollower.registerGSAP(gsap);
+const cursor = new MouseFollower({
+	el: null,
+	container: document.body,
+	className: "mf-cursor",
+	innerClassName: "mf-cursor-inner",
+	textClassName: "mf-cursor-text",
+	mediaClassName: "mf-cursor-media",
+	mediaBoxClassName: "mf-cursor-media-box",
+	iconSvgClassName: "mf-svgsprite",
+	iconSvgStatePrefix: "-",
+	iconSvgSrc: "",
+	dataAttr: "cursor",
+	hiddenState: "-hidden",
+	textState: "-text",
+	iconState: "-icon",
+	activeState: "-active",
+	mediaState: "-media",
+	stateDetection: {
+		"-pointer": "a,button",
+		"-hidden": "iframe",
+	},
+	speed: 0.55,
+	ease: "expo.out",
+	overwrite: true,
+	skewing: 0,
+	skewingText: 2,
+	skewingIcon: 2,
+	skewingMedia: 2,
+	skewingDelta: 0.001,
+	skewingDeltaMax: 0.15,
+	stickDelta: 0.15,
+	showTimeout: 20,
+	showOnEnter: true,
+	hideOnLeave: true,
+	hideTimeout: 300,
+	hideMediaTimeout: 300,
+	initialPos: [-window.innerWidth, -window.innerHeight],
+});
 
 /* İmport SON */
 
