f80a2b01d2e82040b8a9bf5f0142cf64e1682213
Burak <48869708+drementer@users.noreply.github.com>
2022-05-27 04:47:49 +0300

    yeni form elementleri eklendi

--- src/js/components/__form-elemanlari.js
+++ src/js/components/__form-elemanlari.js
@@ -1,25 +1,51 @@
 /* Form Elemanları */
 const form_elemanlari = () => {
 	/* Textarea */
-	const ta_hizala = (ta) => {
-			ta.style.height = ta.scrollHeight + "px";
+	const ta_hizala = (element) => {
+			element.style.height = "auto";
+			element.style.height = element.scrollHeight + "px";
 		},
-		textareas = document.querySelectorAll("textarea");
-	textareas.forEach((ta) => {
+		textarealar = document.querySelectorAll("textarea");
+	textarealar.forEach((textarea) => {
 		// Değer girildiğinde
-		ta.addEventListener("input", () => {
-			// İçeriği silindiğinde
-			if (!ta.value) {
-				ta.style.height = "initial";
-			}
-			// İşlem
-			ta_hizala(ta);
-		});
+		textarea.addEventListener("input", () => {
+			ta_hizala(textarea);
 
-		// Render olduğunda
-		ta.onload = ta_hizala(ta);
+			// İçeriği silindiğinde
+			if (element.value == undefined || element.value == "") {
+				element.style.height = "70px";
+			}
+		});
+		textarea.onload = ta_hizala(textarea);
 	});
 	/* Textarea SON */
+
+	/* İnputlar */
+	const inputlar = document.querySelectorAll(".input");
+	inputlar.forEach((input) => {
+		let input_ogesi =
+			input.querySelector("input") || input.querySelector("textarea");
+		if (input_ogesi) {
+			input_ogesi.addEventListener("input", () => {
+				if (input_ogesi.checkValidity()) {
+					input.removeAttribute("invalid");
+				} else {
+					input.setAttribute("invalid", "");
+				}
+			});
+		}
+	});
+	/* İnputlar SON */
+
+	/* Checkbox & Radio */
+	const elementler = document.querySelectorAll(".checkbox, .radio");
+	elementler.forEach((element) => {
+		let element_input = element.querySelector("input");
+		if (element.hasAttribute("checked")) {
+			element_input.toggleAttribute("checked");
+		}
+	});
+	/* Checkbox & Radio SON */
 };
 /* Form Elemanları SON */
 
