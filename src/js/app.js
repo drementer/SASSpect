d6236c211ea42a11f77f9663657ea5bb4500845c
Burak <48869708+drementer@users.noreply.github.com>
2022-07-14 13:23:41 +0300

    importlar düzenlendi

--- src/js/app.js
+++ src/js/app.js
@@ -8,16 +8,14 @@ import {
 	doc,
 	body,
 } from "./scripts/_utils.js";
-import sliders from "./scripts/_sliders.js";
-import lazy_load from "./vendor/_lazy-load.js";
-import cerez from "./components/_cerez-popup.js";
+import cerez_popup from "./components/_cerez-popup.js";
 import form_elemanlari from "./components/_form-elemanlari.js";
-import got_top from "./components/_go-top.js";
+import yukari_cik from "./components/_go-top.js";
 import loader from "./components/_loader.js";
+import lazy_load from "./vendors/_lazy-load.js";
 
-cerez();
-sliders();
-lazy_load();
+cerez_popup();
 form_elemanlari();
-got_top();
+yukari_cik();
 loader();
+lazy_load();
