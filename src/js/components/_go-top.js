901ed43cda11ad74621fd1b1d294dcc91e1d53ec
Burak <48869708+drementer@users.noreply.github.com>
2022-07-14 13:27:07 +0300

    hat düzenlemeleri ve yorumlama

--- src/js/components/_go-top.js
+++ src/js/components/_go-top.js
@@ -1,18 +1,24 @@
 const go_top = () => {
-	const go_top_buton = document.querySelector(".go-top"),
-		ekran_kontrol = () => {
-			if (window.pageYOffset > window.innerHeight) {
-				go_top_buton.classList.add("go-top--aktif");
-			} else {
-				go_top_buton.classList.remove("go-top--aktif");
-			}
-		},
-		yukari_cik = () => {
-			window.scrollTo({
-				top: 0,
-				behavior: "smooth",
-			});
-		};
+	const go_top_buton = document.querySelector(".go-top");
+
+	const ekran_kontrol = () => {
+		if (window.pageYOffset >= window.innerHeight) {
+			// >= 100vh
+			go_top_buton.classList.add("go-top--aktif");
+		} else {
+			// < 100vh
+			go_top_buton.classList.remove("go-top--aktif");
+		}
+	};
+
+	const yukari_cik = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+	};
+
+	// Buton varsa
 	if (go_top_buton) {
 		// Sayfa yenilendiğinde konum kontrolü için
 		ekran_kontrol();
