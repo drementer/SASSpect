9452709b187fcda37171452e862c592255df9ea3
Burak <48869708+drementer@users.noreply.github.com>
2023-01-27 22:21:25 +0300

    Deleted neverused functions

--- src/js/_utils.js
+++ src/js/_utils.js
@@ -3,22 +3,10 @@
  */
 
 /**
- * Shorthand for document.
- */
-const doc = document;
-
-/**
- * Shorthand for body.
- */
-const body = doc.body;
-
-/**
  * Shorthand for console log.
  * @param {*} log - Console logged value.
  */
-const log = (log) => {
-  console.log(log);
-};
+const { log } = console;
 
 /**
  * Fast add class from element.
@@ -54,53 +42,11 @@ const select = (selector, scope = document) => {
  * @param {object} [scope = document] - The parent of the items to be selected
  * @returns {object} Selected elements
  */
-const selects = (selector, scope = document) => {
+const selectAll = (selector, scope = document) => {
   return scope.querySelectorAll(selector);
-};
-
-/**
- * Setting scroll status function.
- * @param {integer} [status = 1] - Status of scroll is enabled.
- */
-const scroll = (status = 1) => {
-  status === 1
-    ? (body.style.overflowY = 'auto')
-    : (body.style.overflowY = 'hidden');
-};
-
-/**
- * Get element position.
- * @param {object} object - Object for pick position
- * @return {object} Position of object
- */
-const position = (object) => {
-  const box = object.getBoundingClientRect();
-  const docEl = doc.documentElement;
-
-  let scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
-  let scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
-  let clientTop = docEl.clientTop || body.clientTop || 0;
-  let clientLeft = docEl.clientLeft || body.clientLeft || 0;
-  let top = box.top + scrollTop - clientTop;
-  let left = box.left + scrollLeft - clientLeft;
-
-  top = Math.round(top);
-  left = Math.round(left);
-
-  return { top, left };
 };
 
 /*
 	Exports
 */
-export {
-  doc,
-  body,
-  log,
-  select,
-  selects,
-  scroll,
-  addClass,
-  removeClass,
-  position,
-};
+export { log, removeClass, addClass, select, selectAll };
