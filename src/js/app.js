6b8808db1d5fb9394f2315cd7bd3cfff0aab2e72
Burak <48869708+drementer@users.noreply.github.com>
2023-01-27 22:21:37 +0300

    Rebased app

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,6 @@
-/*
-	Imports
-*/
-import {
-  doc,
-  body,
-  log,
-  select,
-  selects,
-  scroll,
-  addClass,
-  removeClass,
-  position,
-} from './_utils.js';
-import cookiePopup from './_cookie-popup.js';
+import { log, removeClass, addClass, select, selectAll } from './_utils.js';
 import formElements from './_form-elements.js';
-import goTop from './_go-top.js';
-import loader from './_loader.js';
 import lazyLoad from './vendors/_lazy-load.js';
 
-/*
-	Inits
-*/
-/* cookiePopup(); */
 formElements();
-goTop();
-loader();
 lazyLoad();
