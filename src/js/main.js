dc6c40e684ad230828ee59ea79bb635717a04382
drementer <48869708+drementer@users.noreply.github.com>
2023-01-28 16:25:56 +0300

    added helper functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,47 @@
-import { log, removeClass, addClass, select, selectAll } from './_utils.js';
 import formElements from './_form-elements.js';
 import lazyLoad from './vendors/_lazy-load.js';
 
-formElements();
-lazyLoad();
+/**
+ * Shorthand for console log.
+ * @param {*} log - Console logged value.
+ */
+const { log } = console;
+
+/**
+ * Fast add class from element.
+ * @param {object} element - Manipulation element
+ * @param {string} className - Class name
+ */
+const addClass = (element, className) => {
+  element.classList.add(className);
+};
+
+/**
+ * Fast remove class from element.
+ * @param {object} element - Manipulation element
+ * @param {string} className - Class name
+ */
+const removeClass = (element, className) => {
+  element.classList.remove(className);
+};
+
+/**
+ * Fast select item.
+ * @param {string} selector - Css selector
+ * @param {object} [scope = document] - The parent of the item to be selected
+ * @return {object} Selected element
+ */
+const select = (selector, scope = document) => {
+  return scope.querySelector(selector);
+};
+
+/**
+ * Fast select items.
+ * @param {string} selector - Css selector
+ * @param {object} [scope = document] - The parent of the items to be selected
+ * @returns {object} Selected elements
+ */
+const selectAll = (selector, scope = document) => {
+  return scope.querySelectorAll(selector);
+};
+
